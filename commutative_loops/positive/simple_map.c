1121107938b9f4fdbc59899cbb613c5f9787ca20
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-08-29 14:35:59 +0100

    add commutativity samples

---
+++ commutative_loops/positive/simple_map.c
@@ -0,0 +1,10 @@
+
+int main() {
+  unsigned int array[] = {3, 5, 15, 99, 11, 33, 5, 67};
+  const size_t N = sizeof(array) / sizeof(unsigned int);
+
+  for (size_t i = 0; i < N; ++i)
+    array[i] = i;
+
+  return 0;
+}
