4fec9a6304e0010da66fb8318929966e023d4ed6
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-09-08 16:50:27 +0100

    fix compilation errors

--- commutative_loops/negative/cross_iteration_deps.c
+++ commutative_loops/negative/cross_iteration_deps.c
@@ -1,13 +1,15 @@
 
+#include <stddef.h>
+
 int main() {
   unsigned int array[] = {3, 5, 15, 99, 11, 33, 5, 67};
   const size_t N = sizeof(array) / sizeof(unsigned int);
   unsigned int k = 0;
 
   for (size_t i = 1; i < N; ++i)
-    A[i] += A[i - 1];
+    array[i] += array[i - 1];
 
-  k = A[3];
+  k = array[3];
 
   return 0;
 }
--- commutative_loops/positive/simple_map.c
+++ commutative_loops/positive/simple_map.c
@@ -1,4 +1,6 @@
 
+#include <stddef.h>
+
 int main() {
   unsigned int array[] = {3, 5, 15, 99, 11, 33, 5, 67};
   const size_t N = sizeof(array) / sizeof(unsigned int);
