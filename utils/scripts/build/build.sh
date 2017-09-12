0c4417d451f43dd383c9c4e0d78e2dba523eabf8
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-09-12 13:30:29 +0100

    add export and build scripts

---
+++ utils/scripts/build/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# initialize configuration vars
+
+SRC_DIR=""
+INSTALL_DIR=""
+
+
+# set configuration vars
+
+if [[ -z $1 ]]; then 
+  echo "error: source directory was not provided" && exit 1
+fi
+
+SRC_DIR=$1
+INSTALL_DIR=${2:-/../install/}
+
+# print configuration vars
+
+echo "info: printing configuration vars"
+echo "info: source dir: ${SRC_DIR}"
+echo "info: install dir: ${INSTALL_DIR}"
+echo ""
+
+
+cmake \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+  -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+  -DCMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
+  -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=On \
+  -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+  ${CMAKE_OPTIONS} \
+  "${SRC_DIR}"
+
+exit $?
+
---
+++ utils/scripts/build/exports_clang_dep.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export CC=clang 
+export CXX=clang++
+
+export LLVMCONFIG=$(which llvm-config)
+
+export LINKER_FLAGS="-Wl,-L$(${LLVMCONFIG} --libdir)" 
+export LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
+
+export BUILD_TYPE=Debug
+
+[[ -z ${Terrace_DIR} ]] && echo "missing Terrace_DIR"
+export Terrace_DIR
+
+export MemProfiler_DIR
+[[ -z ${MemProfiler_DIR} ]] && echo "missing MemProfiler_DIR"
+
+export CommutativityRuntime_DIR
+[[ -z ${CommutativityRuntime_DIR} ]] && echo "missing CommutativityRuntime_DIR"
+
+CMAKE_OPTIONS="-DTerrace_DIR=${Terrace_DIR}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DMemProfiler_DIR=${MemProfiler_DIR}"
+CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCommutativityRuntime_DIR=${CommutativityRuntime_DIR}"
+
+export CMAKE_OPTIONS
+
