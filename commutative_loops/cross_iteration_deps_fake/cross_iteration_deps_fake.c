85c7cf05fb1fd1a6aad628eb9235c9283e4df510
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-09-15 14:55:26 +0100

    add example

--- commutative_loops/cross_iteration_deps_fake/cross_iteration_deps_fake.c
+++ commutative_loops/cross_iteration_deps_fake/cross_iteration_deps_fake.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 int main() {
-  unsigned int array[] = {3, 5, 15, 99, 11, 33, 5, 67};
+  unsigned int array[] = {0};
   const size_t N = sizeof(array) / sizeof(unsigned int);
 
   for (size_t i = 1; i < N; ++i)
