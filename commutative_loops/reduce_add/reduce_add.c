6ff7207debcd0c7bad6968f08dd23f7ce3326201
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-09-15 10:59:54 +0100

    add example

--- commutative_loops/reduce_add/reduce_add.c
+++ commutative_loops/reduce_add/reduce_add.c
@@ -1,12 +1,16 @@
 
+#include <stdio.h>
 #include <stddef.h>
 
 int main() {
   unsigned int array[] = {3, 5, 15, 99, 11, 33, 5, 67};
   const size_t N = sizeof(array) / sizeof(unsigned int);
+  size_t acc = 0;
 
   for (size_t i = 0; i < N; ++i)
-    array[i] = i;
+    acc += array[i];
+
+  fprintf(stderr, "%zu\n", acc);
 
   return 0;
 }
