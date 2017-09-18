3492da6ac96dc3892616ea19ae988fc6d4b5c4d7
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-09-18 15:50:54 +0100

    add example

--- commutative_loops/map_simple/map_simple.c
+++ commutative_loops/map_simple/map_simple.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 int main() {
-  unsigned int array[] = {3, 5, 15, 99, 11, 33, 5, 67};
+  unsigned array[] = {3, 5, 15, 99, 11, 33, 5, 67};
   const size_t N = sizeof(array) / sizeof(unsigned int);
 
   for (size_t i = 0; i < N; ++i)
