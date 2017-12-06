ca874e6307cb33534581e73dd5965dc691a4f5ee
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-12-06 16:06:37 +0000

    augment example

--- commutative_loops/map_simple/map_simple.c
+++ commutative_loops/map_simple/map_simple.c
@@ -5,11 +5,15 @@
 int main() {
   unsigned array[] = {3, 5, 15, 99, 11, 33, 5, 67};
   const size_t N = sizeof(array) / sizeof(unsigned int);
+  unsigned j = 0;
 
-  for (size_t i = 0; i < N; ++i)
+  for (size_t i = 0; i < N; ++i) {
     array[i] = i;
+    j++;
+  }
 
   fprintf(stderr, "%u\n", array[N / 2]);
+  fprintf(stderr, "%u\n", j);
 
   return 0;
 }
