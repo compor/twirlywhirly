d7996052c3e78c43e51bbcc8fc47e5e32e5c55f7
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-12-09 18:39:58 +0000

    update example

--- commutative_loops/map_simple/map_simple.c
+++ commutative_loops/map_simple/map_simple.c
@@ -6,14 +6,17 @@ int main() {
   unsigned array[] = {3, 5, 15, 99, 11, 33, 5, 67};
   const size_t N = sizeof(array) / sizeof(unsigned int);
   unsigned j = 0;
+  unsigned k = 0;
 
   for (size_t i = 0; i < N; ++i) {
     array[i] = i;
     j++;
+    k += i;
   }
 
   fprintf(stderr, "%u\n", array[N / 2]);
   fprintf(stderr, "%u\n", j);
+  fprintf(stderr, "%u\n", k);
 
   return 0;
 }
