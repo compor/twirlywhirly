5746f8247420c651611512d8bfdc7a3baaa415e9
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-12-19 12:02:59 +0000

    update build export script

--- utils/scripts/source_tree/exports_clang_dep.sh
+++ utils/scripts/source_tree/exports_clang_dep.sh
@@ -13,24 +13,29 @@ export BUILD_TYPE=Debug
 export MemProfiler_DIR
 [[ -z ${DecoupleLoopsFront_DIR} ]] && echo "missing DecoupleLoopsFront_DIR"
 
-[[ -z ${Terrace_DIR} ]] && echo "missing Terrace_DIR"
 export Terrace_DIR
+[[ -z ${Terrace_DIR} ]] && echo "missing Terrace_DIR"
 
 export MemProfiler_DIR
 [[ -z ${MemProfiler_DIR} ]] && echo "missing MemProfiler_DIR"
 
-export Terrace_DIR
-[[ -z ${Terrace_DIR} ]] && echo "missing Terrace_DIR"
-
 export CommutativityRuntime_DIR
 [[ -z ${CommutativityRuntime_DIR} ]] && echo "missing CommutativityRuntime_DIR"
 
-CMAKE_OPTIONS="-DTerrace_DIR=${Terrace_DIR}"
-CMAKE_OPTIONS="${CMAKE_OPTIONS} -DMemProfiler_DIR=${MemProfiler_DIR}"
-CMAKE_OPTIONS="${CMAKE_OPTIONS} -DDecoupleLoopsFront_DIR=${DecoupleLoopsFront_DIR}"
-CMAKE_OPTIONS="${CMAKE_OPTIONS} -DTerrace_DIR=${Terrace_DIR}"
-CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCommutativityRuntime_DIR=${CommutativityRuntime_DIR}"
+
 CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_INSTALL_RPATH=$(${LLVMCONFIG} --libdir)"
+
+[[ -z ${DecoupleLoopsFront_DIR} ]] \
+  && CMAKE_OPTIONS="${CMAKE_OPTIONS} -DDecoupleLoopsFront_DIR=${DecoupleLoopsFront_DIR}"
+
+[[ -z ${Terrace_DIR} ]] \
+  && CMAKE_OPTIONS="${CMAKE_OPTIONS} -DTerrace_DIR=${Terrace_DIR}"
+
+[[ -z ${MemProfiler_DIR} ]] \
+  && CMAKE_OPTIONS="${CMAKE_OPTIONS} -DMemProfiler_DIR=${MemProfiler_DIR}"
+
+[[ -z ${CommutativityRuntime_DIR} ]] \
+  && CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCommutativityRuntime_DIR=${CommutativityRuntime_DIR}"
 
 export CMAKE_OPTIONS
 
